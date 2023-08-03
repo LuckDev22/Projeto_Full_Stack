e6ca2239103bb545105fdc39cb2894669868827d
Lucas Ribeiro <103015314+LuckDev22@users.noreply.github.com>
2023-08-03 09:27:56 -0300

    feat: add login page

--- front/src/components/Header/index.tsx
+++ front/src/components/Header/index.tsx
@@ -1,40 +1,35 @@
-import { MdShoppingCart, MdLogout } from 'react-icons/md';
-import SearchForm from './SearchForm';
-import { StyledHeader } from './style';
-import LogoKenzieBurguer from '../../assets/LogoKenzieBurguer.svg';
-import { StyledContainer } from '../../styles/grid';
-import { useContext } from 'react';
-import { UserContext } from '../../providers/UserProvider';
-import { HomeContext } from '../../providers/HomeContext';
+import { MdShoppingCart, MdLogout } from "react-icons/md";
+
+import { StyledHeader } from "./style";
+import LogoKenzieBurguer from "../../assets/LogoKenzieBurguer.svg";
+import { StyledContainer } from "../../styles/grid";
+// import { useContext } from "react";
+// import { UserContext } from "../../providers/UserProvider";
 
 const Header = () => {
-  const { userLogout } = useContext(UserContext);
-  const { setCartModal } = useContext(HomeContext);
+    // const { userLogout } = useContext(UserContext);
 
-  return (
-    <StyledHeader>
-      <StyledContainer containerWidth={1300}>
-        <div className='flexGrid'>
-          <img
-            src={LogoKenzieBurguer}
-            alt='Kenzie Burguer Logo'
-            className='logo'
-          />
-          <nav className='nav' role='navigation'>
-            <SearchForm />
-            <div className='buttons'>
-              <button type='button' onClick={() => setCartModal(true)}>
-                <MdShoppingCart size={28} />
-              </button>
-              <button onClick={() => userLogout()} type='button'>
-                <MdLogout size={28} />
-              </button>
-            </div>
-          </nav>
-        </div>
-      </StyledContainer>
-    </StyledHeader>
-  );
+    return (
+        <StyledHeader>
+            <StyledContainer containerWidth={1300}>
+                <div className="flexGrid">
+                    <img
+                        src={LogoKenzieBurguer}
+                        alt="Kenzie Burguer Logo"
+                        className="logo"
+                    />
+                    <nav className="nav" role="navigation">
+                        <div />
+                        <div className="buttons">
+                            <button onClick={() => userLogout()} type="button">
+                                <MdLogout size={28} />
+                            </button>
+                        </div>
+                    </nav>
+                </div>
+            </StyledContainer>
+        </StyledHeader>
+    );
 };
 
 export default Header;
